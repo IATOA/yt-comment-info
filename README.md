b151ecdf42d81205e7b0b1dfd15a523b4b7b8478
IATOA <iatoa.yt@gmail.com>
2025-11-04 14:23:19 +0100

    Uploaded the changes I worked on my machine
    Adding all features I need, a way to show the new features in readme, changing the name in the manifest to beter reflect new functions.

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-# yt-comment-sub-count
-A Browser extension which adds the number of YouTube subscribers next to comments.
+# yt-comment-info
+A Chrome/Firefox extension which adds the number of YouTube subscribers, videos, titles of said videos and more! 
 
-
+<!---
 [Link to Chrome Extension](https://chromewebstore.google.com/detail/subscriber-count-on-comme/legnlfnnpadijhghmeagaafplkeljhki)
 
 [Link to Firefox Add-on](https://addons.mozilla.org/en-US/firefox/addon/yt-comment-sub-count/)
+--->
 
 
+![Preview](./resources/preview.png)
 
-![Screenshot](./resources/screenshot.png)
+![Videos](./resources/videos-preview.png)
+
+![Thumbnail](./resources/thumbnail-preview.png)
--- src/js/content.js
+++ src/js/content.js
@@ -7,6 +7,13 @@
    * @param {string} channelUrl the url of a given youtube channel.
    * Should be of the form: https://www.youtube.com/channel/<channel id>
    */
+
+  // I tried but I failed adding proper javascript that would do that
+  // I will bodge in the hover functionality with a style tag in the head element
+
+  const cssHack = "<style>.description{display:none;}.tiptext:hover>.description{display:inline-block;}</style>";
+  document.head.insertAdjacentHTML("beforeend", cssHack);
+
   const getSubs = async (channelUrl) => {
     // It appears that occasionally the channelUrl is missing the https:// prefix.
     // As of 2025-01-31, this occurs on community post comments.
@@ -22,17 +29,170 @@
     // a rendered string in a field called "subscriberCountText".
     // e.g. ... "subscriberCountText":"12.4K subscribers" ...
 
-    // We just do a regex to find it for now
-    const regex = /"subscriberCountText":"([^"]+)"/;
-    const match = text.match(regex);
+    // Added other regexes covering everything I want to include:
+    const regexSubCount = /"subscriberCountText":"([^"]+)"/;
+    const regexVidCount = /"videoCountText":"([^"]+)"/;
+
+    // We extract the info above from the "/about" box/
+    // Info below is regexed from other places on the page,
+    // "\} in viewCountText is there to filter out shorts from standard videos
+    // we use ? to filter out rss feed channel id
+
+    const regexVidName        = /"title":\{"accessibility":\{"accessibilityData":\{"label":"((?:(?:\\")|[^"])+)"/;
+    const regexTitles         = /"title":\{"accessibility":\{"accessibilityData":\{"label":"((?:(?:\\")|[^"])+)"/g;
+    // This will be cleaned up I just want ts to work, grabbing appropriate viddate/vidviews more consistantly
+    const fullInfoRegex       = /"title":\{"accessibility":\{"accessibilityData":\{"label":"(?:(?:(?:\\")|[^"])+)"\}\},"simpleText":"(?:(?:(?:\\")|[^"])+)"\},"publishedTimeText":\{"simpleText":"([^"]+)"\},"viewCountText":\{"simpleText":"([^"]+)"/
+    // This is used for rare situations where there is no "Videos" box.
+    const regexVidDate        = /"publishedTimeText":\{"simpleText":"([^"]+)"/;
+    const regexVidViews       = /"\},"viewCountText":\{"simpleText":"([^"]+)"/;
+    const regexID             = /\?channel_id=([^"]+)/;
+
+    const regexNewVidID = /[^\]]\},"content":\{"horizontalListRenderer":\{"items":\[\{"gridVideoRenderer":\{"videoId":"([^"]+)"/;
+    var matchNewVidID = text.match(regexNewVidID);
+    var nulltestNewVidID = matchNewVidID ? matchNewVidID[1] : "(?!.*)";
+
+    //Shorts are horizontal, normal vids are not, this can swap if needed
+    var thumbnailHeight = 105;
+    var thumbnailWidth =  140;
+
+    // We grab the subscriberCountText field.
+    const SubCount = text.match(regexSubCount);
 
     // User has set their subscriber count to private.
     // We detect this if the subscriberCountText field is missing.
-    if (match === null) return '<i>Private</i>';
+    // We use a ? : to check if the SubCount is null, if so we return "Private"
+    // else we force it into a new variable
+    const SubCountText = SubCount ? SubCount[1] : '<i>Private</i>';
 
-    // We now have a string like: "subscriberCountText":"2 subscribers"
-    // with the count in match group 1, so return just that.
-    return match[1];
+    //We grab the videoCountText field.
+    const VidCount = text.match(regexVidCount);
+
+    // User has no videos.
+    // We detect this if the videoCountText field is missing.
+    if (VidCount === null){
+        return SubCountText + ' | 0 videos';
+    } else {
+        // We find the hidden playlist with all videos manipulating channelID
+        // We will use it later when we will create a link to it
+        // We will use the ID later.
+        var matchID = text.match(regexID);
+        const playlist = matchID[1].replace('UC', 'https://www.youtube.com/playlist?list=UU');
+
+        // I still decided to do this because matchAll returns an iterator
+        // it blinks when function is looping morover, it's ugly, moreover it's a cleaner
+        // way to get a null value on vidname if nothing gets caught
+        var VidName = text.match(regexVidName);
+        var titles = text.matchAll(regexTitles);
+        // We set this flag for use later to differentiate if the user has shorts only
+        var shorts = 0;
+        var VidViews = ""
+        var finalVidViews = "";
+        var finalVidDate = "";
+        const regexShortClean = /,\s([^,]+)\s[–-]\s[^–-]+$/;
+
+        if (VidName === null) {
+           // We apply those regexes for shorts, and /videos like layout on the main page
+           const regexShortsTitles = /"entityId":"shorts-shelf-item-(?:[^"]+)","accessibilityText":"((?:(?:\\")|[^"])+)"/g;
+           const shortsTitles = text.matchAll(regexShortsTitles);
+           titles = shortsTitles;
+           const regexShortsInfo = /"entityId":"shorts-shelf-item-([^"]+)","accessibilityText":"((?:(?:\\")|[^"])+)","thumbnail":\{"sources":\[\{"url":"([^"]+)"/;
+           const shortsInfo = text.match(regexShortsInfo);
+           if (shortsInfo !== null){
+             shorts = 1;
+             thumbnailHeight = 140;
+             thumbnailWidth =  105;
+             nulltestNewVidID = shortsInfo[1];
+             shortsClean = shortsInfo[2].match(regexShortClean);
+             VidName = ["", shortsInfo[2].replace(shortsClean[0], "")];
+             finalVidThumbnail = shortsInfo[3];
+             finalVidViews = shortsClean[1];
+           } else {
+             // this is super silly, it filters out Avatar Image (88px), will clean it up later if i get around to it
+             const regexVidNameAlt = /[^8{2}]\}\],"accessibility":\{"accessibilityData":\{"label":"((?:(?:\\")|[^"])+)"/;
+             const regexPlaylistTitlesAlt = /\}\],"accessibility":\{"accessibilityData":\{"label":"((?:(?:\\")|[^"])+)"/g;
+             const regexNewVidIDAlt = /{"videoRenderer":\{"videoId":"([^"]+)"/;
+             VidName = text.match(regexVidNameAlt);
+             titles = text.matchAll(regexPlaylistTitlesAlt);
+             const newVidIDAlt = text.match(regexNewVidIDAlt);
+             nulltestNewVidID = newVidIDAlt ? newVidIDAlt[1] : "";
+
+             // // It seems too much, for the fraction of a fraction of channels that will fail all the other attempts to scrape.
+             // // For now at least. Also I would need to scrape vid views/vid date differently
+             // if (VidName === null){
+             //    const playlistResponse = await fetch(playlist);
+             //    const playlistText = await playlistResponse.text();
+             //    console.log(playlistText);
+             //    const regexPlaylistTitles = /\}\],"accessibility":\{"accessibilityData":\{"label":"((?:(?:\\")|[^"])+\d[^"]+)"\}\}\},/g;
+             //    const regexNewVidIDAlt = /\{"playlistVideoRenderer":\{"videoId":"([^"]+)"/;
+             //    VidName = playlistText.match(regexPlaylistTitles);
+             //    console.log(VidName);
+             //    titles = playlistText.matchAll(regexPlaylistTitles);
+             //    const testNewVidID = playlistText.match(regexNewVidIDAlt);
+             //    console.log(nulltestNewVidID);
+             // }
+           };
+         };
+
+        const stringVidThumbnail = "https://i.ytimg.com/vi/" + nulltestNewVidID + "(:?[^\"]+)";
+        const regexVidThumbnail  = new RegExp(stringVidThumbnail, "");
+        const matchVidThumbnail = text.match(regexVidThumbnail);
+        var finalVidThumbnail = matchVidThumbnail ? matchVidThumbnail[0] : "";
+
+        //Using /regexpr/g and matchAll we get an object that can be iterated on
+        //We plug it into this fuction, repeat it 20 times and then "break"
+        // "&#010;" is an html code for a newline, so title+newline+title+newline etc.
+        var forloopoutput = "";
+        var forIter = 0;
+        for (const title of titles) {
+           if (shorts == 1){
+             // We use this short regex to remove the "- Watch Shorts" text
+             const matchShortClean = title[1].match(regexShortClean);
+             title[1] = title[1].replace(matchShortClean[0], "");
+           }
+          //This lets us avoid issues with parcing " in title name
+          forloopoutput += title[1].replaceAll("\\\"", "&quot;") + "&#010;";
+          forIter += 1;
+          if (forIter > 19) break;
+        }
+
+        // We create and style videocount to behave as a link to our hidden playlist
+        // We add a newline seperated list of titles created by the for loop above on hover
+        const textVidCount = "<a style=\"color:#ddd;\" href=\""+ playlist + "\" title=\"" + forloopoutput + "\">" + VidCount[1] + "</a>";
+
+        // All scraping attemps failed but videos exist, for now we metion this fact.
+        if (VidName === null) {
+          return SubCountText + ' | ' + textVidCount + ' | fail';
+        } else {
+        if (shorts == 0) {
+          const nulltestVidViews = text.match(regexVidViews) ? text.match(regexVidViews)[1] : "error";
+          const nulltestVidDate = text.match(regexVidDate) ? text.match(regexVidDate)[1] : "error";
+          const VidInfo = text.match(fullInfoRegex) ? text.match(fullInfoRegex) : ["", nulltestVidViews, nulltestVidDate];
+          finalVidDate = VidInfo[1];
+          finalVidViews = VidInfo[2];
+        };
+
+        // We do this regex to seperate the name from the title
+        // (?:foo)      treat all those searches as a block, don't output seperately
+        //\s            whitespace character (space)
+        //[0-9]{1,2}    0-99
+        //\S*           0 or any number of non-whitespace characters
+        //[^\d\s]+      1+ of a [NOT \d = 0-9 and \s = whitespace ]
+        //foo?          "foo" or a lack of "foo"
+        //$             end of string
+        // Bugs be plenty with this naive way of doing it, chinese, arabic are broken
+        const regexVidLenght = /(?:\s?[0-9]{1,2}\s?[^\d\s]+)?(?: [^\d\s]|,)? [0-9]{1,2} [^\d\s]+$/;
+        const VidLenght = VidName[1].match(regexVidLenght) ? VidName[1].match(regexVidLenght) : "short";
+        const textVidName = VidName[1].replace(VidLenght, '').replaceAll('\\','');
+
+
+        // Now we have matches for all the videos.
+        // We return match group 1 because that's where the newest video will be (like 60% of the time).
+
+        const hoverVidThumbnail = "<div class=\"description\"  style=\"position:absolute;border:1px solid #000;width:" + thumbnailWidth + "px;height:" + thumbnailHeight + "px;inset-block-start:-" + thumbnailHeight + "px;background-image:url("+ finalVidThumbnail +");background-size:cover;\"></div>";
+        const finalVidName = "<a class=\"tiptext\" style=\"color:#ddd;text-decoration:none;\" href=\"https\://youtu.be/" + nulltestNewVidID + "\">" + hoverVidThumbnail + textVidName + "</a>";
+        return SubCountText + ' | ' + textVidCount + ' | <b>' + finalVidName + '</b> | ' + VidLenght + ' | '+ finalVidViews + ' | ' + finalVidDate;
+      };
+    };
   };
 
   /**
